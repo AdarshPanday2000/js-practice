7ddbac47f9a228e966e61cb3ec25795376357511
Adarsh Panday <ap3497017@gmail.com>
2023-10-20 21:49:36 +0530

    arrays

--- array.js
+++ array.js
@@ -188,3 +188,421 @@
 // console.log(leaders([10,22,12,3,0,6]))
 
 
+// function divisor(n){
+//     let ans = []
+//     for(let i = 1; i*i <= n; i++){
+//         if(n % i === 0){
+//             if(i !== n/i){
+//                 ans.push(i, n/i)
+//             }else{
+//                 ans.push(i)
+//             }
+//         }
+//     }
+//     return ans;
+// }
+// console.log(divisor(36));
+
+//fibonacci
+// function fibonacci(n){
+// //     if( n === 0 ) return 0;
+// //     if(n === 1)return 1;
+// //     return fibonacci(n-1) + fibonacci(n-2)
+// // }
+// let ans = [0,1]
+
+// for(let i = 2; i<= n; i++){
+//     ans[i] = ans[i-1]+ans[i-2]
+// }
+// return ans
+// }
+// console.log(fibonacci(6));
+
+// function leftRotate (arr){
+//     for(let i = 1; i < arr.length; i++){
+//         [arr[i],arr[i-1]]= [arr[i-1], arr[i]]
+//     }
+//     return arr;
+// }
+// console.log(leftRotate([1,2,3,4,5]))
+
+// function leftRotate (arr,k){
+
+//     let n =arr.length
+//     reverse(arr,0,k-1)
+//     reverse(arr,k,n-1)
+//     reverse(arr,0,n-1);
+
+//     return arr;
+// }
+// function reverse(arr,s,e){
+//     while(s < e){
+//         [arr[s],arr[e]] = [arr[e],arr[s]]
+//         s++
+//         e--;
+//     }
+// }
+// console.log(leftRotate([1,2,3,4,5],2));
+
+// function majority (arr){
+//     let ans = []
+//     let n = arr.length;
+//     let hash = new Array(Math.max(...arr)+1)
+//     hash.fill(0);
+
+//     for(let i = 0; i< n; i++){
+//         hash[arr[i]]++
+//     }
+//     for(let i = 0; i< hash.length; i++){
+//         if(hash[i] > Math.floor(n/3)){
+//             ans.push(i)
+//         }
+//     }
+//     return ans;
+// }
+// console.log(majority([1,1,1,3,3,2,2,2]))
+// console.log(majority([-1,-1,-1]))
+
+//find the number of roatations
+// function rotated (arr){
+//     let n = arr.length
+//     let low = 0
+//     let high = n-1
+//     let ans = Infinity
+//     let index = -1;
+
+//     while(low <= high){
+//         let mid = Math.floor((low+high)/2)
+//         if(arr[low] <= arr[mid]){
+//             if(arr[low] < ans){
+//                 index = low;
+//                 ans = arr[low]
+//             }
+//             low = mid+1
+//         }
+
+//         else{
+//             if(arr[mid] <= arr[high]){
+//                 index = mid;
+//                 ans = arr[mid]
+//             }
+//             high = mid -1
+//         }
+//     }
+//    // return ans;
+//     return index;
+// }
+
+// console.log(rotated([3,4,5,1,2]));
+
+// function rotatedSorted(arr){
+//     let n = arr.length
+//     let rotatedIndex = -1
+    
+//     //find index of biggest rotated element
+//     for(let i = 1; i < n; i++){
+//         if(arr[i] <= arr[i+1]){
+//             rotatedIndex = i+1
+//             console.log(rotatedIndex
+//                 )
+//             break;
+//         }
+//     }
+
+//     //check from 0 to rotatedindex that is it ascending
+//     for(let i = 0; i <= rotatedIndex; i++){
+//         if(arr[i] < arr[i+1]){
+//             return true
+//         }
+//         else return false;
+//     }
+
+//     //check from rotated to last that is it ascending
+//     for(let i = rotatedIndex + 1; i < n; i++){
+//         if (arr[i] < arr[i+1]){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// };
+// 1,2,3,4   4,1,2,3  3,4,1,2  2,3,4,1  
+// function rotatedSorted(arr){
+//     let n = arr.length
+//     let minIndex = -1
+//     let minValue = Infinity
+//     for(let i = 0; i<n ; i++){
+//         minIndex = Math.
+//         minValue = Math.min(minValue,arr[i])
+//     }
+// }
+
+// console.log(rotatedSorted([4,5,6,7,1,2,3]));
+// console.log(rotatedSorted([5,6,7,1,4,5,2,3]));
+
+//ans = 0
+//[1,2,3,1,1,1,1,2,3], k =5
+// i
+//sum=0
+//    j
+//sum += arr[j]
+// if(sum = k) max(j-i+1,ans)
+
+
+//arr[1,1,2,2,2,3,3]
+//ans = [1,2,3]
+
+// function remove(arr){
+//     let i = 0
+//     for(let j = 1; j<arr.length; j++){
+//         if(arr[j] !== arr[i]){
+//             i++;
+//             arr[i] = arr[j]
+//         }
+//     } 
+//     i++
+//     while(i < arr.length){
+//         arr[i] = '_'
+//         i++
+//     }
+//     return arr;
+    
+// }
+// console.log(remove([1,1,2,2,3,3,3]))
+
+
+// function removeDup(arr){
+//     let set = new Set(arr)
+//     let temp = [...set]
+//     return temp
+// }
+// console.log(removeDup([1,1,2,2,3,3,3]))
+
+
+//arr1 =[1,2,3,4,5]
+//arr2= [2,3,4,4,5]
+
+//ans=[1,2,3,4,5]
+
+// function union(arr1,arr2){
+//     let ans = arr1.concat(arr2);
+//     let set = new Set(ans);
+//     let temp = [...set]
+//     return temp;
+// }
+// console.log(union([1,2,3,4,5], [2,3,4,4,5]));
+
+// function findUnion(arr1,arr2){
+//     let ans = []
+//     let i = 0
+//     let j = 0
+//     while(i <= arr1.length && j <arr2.length){
+//         if(arr1[i] <= arr2[j]){
+//             if(ans.length === 0 || ans[ans.length-1] !==arr1[i]){
+//                 ans.push(arr1[i])
+                
+//             }
+//             i++;           
+//         }
+//         else{
+//             if(ans.length === 0 || ans[ans.length-1] !==arr2[j]){
+//                 ans.push(arr2[j])
+//             }
+//             j++;
+//         }
+//     }
+//     while(i < arr1.length){
+//         if(ans[ans.length-1] !== arr1[i]){
+//             ans.push(arr1[i])
+//         }
+//     }
+//     while(j < arr2.length){
+//         if(ans[ans.length-1] !== arr2[j]){
+//             ans.push(arr2[j])
+//         }
+//     }
+//     return ans;
+// };
+
+// console.log(findUnion([1,2,3,4,5], [2,3,4,4,5]));
+
+
+// function maxOnes(arr){
+//     let count = 0
+//     let output = 0
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] === 1){
+//             count += 1
+//         }
+//         else if(arr[i] == 0){
+//             output = count
+//             count = 0
+//         }
+//          output = Math.max(output,count)
+//     }
+//     return output;
+// }
+
+// console.log(maxOnes([1,1,0,1,1,1]))
+
+
+// function secondLargest(arr){
+//     let largest = arr[0]
+//     let SL = -Infinity 
+//     for(let i = 1; i<arr.length ; i++){
+//         if(arr[i] > largest){
+//             SL = largest
+//             largest = arr[i]
+//         }
+//         else if(arr[i] < largest && arr[i]>SL){
+//             SL = arr[i]
+//         }
+//     }
+//     return SL;
+// }
+// console.log(secondLargest([1,2,3,7,7,5]))
+
+// function secondSmallest(arr){
+//     let smallest = arr[0]
+//     let secondSmall = Infinity
+//     let n = arr.length
+    
+//     for(let i = 1; i < n; i++){
+//         if (arr[i] < smallest){
+//             secondSmall = smallest
+//             smallest = arr[i]
+//         }
+//         else if(arr[i] !== smallest && arr[i] < secondSmall){
+//             secondSmall = arr[i]
+//         }
+//     }
+//     return secondSmall;
+// }
+// console.log(secondSmallest([3,1,4,5,2,3,6]))
+
+// function isSorted(arr){
+//     for(let i = 1; i< arr.length ; i++){
+//         if(arr[i] >= arr[i-1]){
+//             //return true
+//         }
+//         else return false
+//     }
+//     return true
+// }
+// console.log(isSorted([1,2,3,7,5]))
+
+
+// function duplicates(arr){
+//     let i = 0 
+//     for(let j = 1 ; j< arr.length; j++){
+//         if(arr[i] !== arr[j]){
+//             arr[i+1] = arr[j]
+//             i++
+//         }
+//     }
+//     return i+1;
+// }
+// console.log(duplicates([1,1,2,2,3,3]))
+
+
+// function leftSingleRotate(arr){
+//     let n = arr.length
+//     let temp = arr[0]
+//     for(let  i = 1; i< n ; i++){
+//         arr[i-1] = arr[i]
+//     }
+//     arr[n-1] = temp ;
+//     return arr;  
+// }  
+// console.log(leftSingleRotate([1,2,3,4,5]))
+
+
+// function dLeftRotate(arr,d){
+//     let n = arr.length
+//     reverse(arr,0,d-1)
+//     reverse(arr,d,n-1)
+//     reverse(arr,0,n-1)
+//     return arr;
+// }
+// function reverse(nums,s,e){
+//     //let s = 0
+//     //let e = nums.length-1
+//     while(s < e){
+//         [nums[s], nums[e]] = [nums[e], nums[s]]
+//         s++
+//         e--
+//     }
+// }
+// console.log(dLeftRotate([1,2,3,4,5,6,7],3))
+
+// function zeroAtEnd(arr){
+
+//     //find the first 0th index
+//     for(let i = 0 ; i< arr.length ; i++){
+//         if(arr[i] === 0){
+//             j = i;
+//             break;
+//         }
+//     }
+//     //loop from i to lasr swap elem
+//     for(let i = j+1; i<arr.length; i++){
+//         if(arr[i] !== 0){
+//             [arr[i], arr[j]] = [arr[j], arr[i]]
+//             j++
+//         }
+//     }
+//     return arr;
+// }
+// console.log(zeroAtEnd([1,0,2,3,2,0,0,4,5,1]))
+
+
+// function maxOnes(arr){
+//     let max = 0
+//     let count = 0
+
+//     for(let i = 0; i <arr.length; i++){
+//         if(arr[i] == 1){
+//             count++
+//             max = Math.max(count,max)
+//         }
+//         else{
+//             count = 0
+//         }
+//     }
+//     return max;
+// }
+// console.log(maxOnes([1,1,0,1,1,1,1,0,1,1]))
+
+
+// function numberOnce(arr){
+//     let hash = new Array(Math.max(...arr) + 1).fill(0);
+
+//     for(let i = 0; i<arr.length ; i++){
+//         hash[arr[i]]++
+//     }
+//     for(let i = 0; i<hash.length; i++){
+//         if(hash[arr[i]] == 1){
+//             return arr[i]
+//         }
+//     }
+// }
+// console.log(numberOnce([1,1,2,2,3,4,4]))
+
+
+function longestSubarray(arr,k){
+    // let length = 0
+    // for(let i = 0; i< arr.length ; i++){
+    //     let sum = 0
+    //     for(let j = i; j< arr.length; j++){
+    //         sum += arr[j]
+    //         if(sum ===  k){
+    //             length = Math.max(length, j-i+1);
+    //         }
+    //     }
+    // }
+    // return length;
+
+    
+}
+console.log(longestSubarray([2,3,5,1,9],10))
+
